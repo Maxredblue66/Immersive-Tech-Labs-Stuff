47887db8fc33a89aff3510cf08145a47360905bb
Maxredblue66 <maxredblue66@o2.pl>
2026-03-09 11:00:28 +0000

    finished script to automatically use fps player

--- Assets/Scripts/HMD_Manager.cs
+++ Assets/Scripts/HMD_Manager.cs
@@ -3,15 +3,28 @@ using UnityEngine.XR;
 
 public class HMD_Manager : MonoBehaviour
 {
+
+    [SerializeField] GameObject xrPLayer;
+    [SerializeField] GameObject fpsPLayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Using Device:" + XRSettings.loadedDeviceName);
-    }
+        if (XRSettings.isDeviceActive)
+        {
+            fpsPLayer.SetActive(false);
+            xrPLayer.SetActive(true);
+        }
+        else
+        {
+            xrPLayer.SetActive(false);
+            fpsPLayer.SetActive(true);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
